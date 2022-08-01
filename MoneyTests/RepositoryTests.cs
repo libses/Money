061f112b80b706d00f141cd06e5bc34ac9585ace
da.morozov <da.morozov@skbkontur.ru>
2022-08-01 11:28:43 +0500

    refac tests

--- MoneyTests/RepositoryTests.cs
+++ MoneyTests/RepositoryTests.cs
@@ -7,14 +7,25 @@ namespace MoneyTests
 {
     public class RepositoryTests
     {
-        [Test]
-        public void OrdinaryCaseMustReturnCorrect()
+        public Repository Repository;
+
+        [SetUp]
+        public void SetUp()
         {
-            var repo = new Repository();
-            repo.SetMoneyAmount(10000);
-            var today = new DateTime(2007, 9, 8, 12, 11, 11);
-            repo.AddRegularCashFlow(new RegularCashFlow(10000, new Monthly(10)));
-            repo.GetDailyBudgetToNextRegular(today).Should().Be(5000);
+            Repository = new Repository();
+            Repository.SetMoneyAmount(10000);
+        }
+        
+        [TestCase(2007, 9, 8, 12, 11, 11, 10, 5000, Description = "Ordinary")]
+        [TestCase(2007, 12, 31, 11, 11, 11, 1, 10000, Description = "New year")]
+        public void GetDailyBudgetOrdinaryMonthly(int year, int month, int day,
+            int hour, int minute, int second,
+            int dayOfMonth,
+            int expected)
+        {
+            var today = new DateTime(year, month, day, hour, minute, second);
+            Repository.AddRegularCashFlow(new RegularCashFlow(10000, new Monthly(dayOfMonth)));
+            Repository.GetDailyBudgetToNextRegular(today).Should().Be(expected);
         }
     }
 }
